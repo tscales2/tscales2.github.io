c3418647cb15efcb62597bdec5495783f008e7cb
tscales2 <takinahmscales@gmail.com>
2019-12-23 16:08:39 -0500

    updated site with more files

--- numberGame.js
+++ numberGame.js
@@ -1,75 +1,75 @@
 var computerGuess;
 var userGuessLog = [];
 var attempts = 0;
-var maxGuesses = 10;
+var maxGuesses = 20;
 
 function gameEnded() {
-  document.getElementById('newGameButton').style.display = 'inline';
-  document.getElementById('easyBtn').style.display = 'none';
-  document.getElementById('hardBtn').style.display = 'none';
-  document.getElementById('inputBox').setAttribute('readonly', 'readonly');
+    document.getElementById('newGameButton').style.display = 'inline';
+    document.getElementById('easyBtn').style.display = 'none';
+    document.getElementById('hardBtn').style.display = 'none';
+    document.getElementById('inputBox').setAttribute('readonly', 'readonly');
 }
 
 function easyMode() {
-  maxGuesses = 10;
-  document.getElementById('easyBtn').className = 'activeButton';
-  document.getElementById('hardBtn').className = '';
+    maxGuesses = 20;
+    document.getElementById('easyBtn').className = 'activeButton';
+    document.getElementById('hardBtn').className = '';
 }
 
 function hardMode() {
-  maxGuesses = 5;
-  document.getElementById('hardBtn').className = 'activeButton';
-  document.getElementById('easyBtn').className = '';
+    maxGuesses = 10;
+    document.getElementById('hardBtn').className = 'activeButton';
+    document.getElementById('easyBtn').className = '';
 }
 
 function newGame() {
-  window.location.reload();
+    window.location.reload();
 }
 
 function init() {
-  computerGuess = Math.floor(Math.random() * 100 + 1);
-  // console.log(computerGuess);
-  document.getElementById('newGameButton').style.display = 'none';
+    computerGuess = Math.floor(Math.random() * 1000 + 1);
+    // console.log(computerGuess);
+    document.getElementById('newGameButton').style.display = 'none';
 }
 
 function compareGuess() {
-  var userGuess = " " + document.getElementById('inputBox').value;
-  // console.log(userGuess);
+    var userGuess = " " + document.getElementById('inputBox').value;
+    // console.log(userGuess);
 
-  userGuessLog.push(userGuess);
-  // console.log(userGuessLog);
-  document.getElementById('guessLog').innerHTML = userGuessLog;
+    userGuessLog.push(userGuess);
+    // console.log(userGuessLog);
+    document.getElementById('guessLog').innerHTML = userGuessLog;
 
-  attempts++;
-  document.getElementById('attempts').innerHTML = attempts;
+    attempts++;
+    document.getElementById('attempts').innerHTML = attempts;
 
-  if(userGuessLog.length < maxGuesses) {
-    if(userGuess > computerGuess) {
-      document.getElementById('textOutput').innerHTML = 'Your guess is too high';
-      document.getElementById('inputBox').value = "";
-    } else if (userGuess < computerGuess) {
-      document.getElementById('textOutput').innerHTML = 'Your guess is too low';
-      document.getElementById('inputBox').value = "";
+    if (userGuessLog.length < maxGuesses) {
+        if (userGuess > computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'Your guess is too high';
+            document.getElementById('inputBox').value = "";
+        } else if (userGuess < computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'Your guess is too low';
+            document.getElementById('inputBox').value = "";
+        } else {
+            document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' + attempts + ' attempts';
+            document.getElementById('container').style.backgroundColor = 'green';
+            gameEnded();
+        }
     } else {
-      document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' +attempts+ ' attempts';
-      document.getElementById('container').style.backgroundColor = 'green';
-      gameEnded();
+        if (userGuess > computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'You lose!' + '<br> the number was ' + computerGuess;
+            document.getElementById('container').style.backgroundColor = '#e82c4e';
+            gameEnded();
+        } else if (userGuess < computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'You lose!' + '<br> the number was ' + computerGuess;
+            document.getElementById('container').style.backgroundColor = '#e82c4e';
+            gameEnded();
+        } else {
+            document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' + attempts + ' attempts';
+            document.getElementById('container').style.backgroundColor = 'green';
+            gameEnded();
+        }
     }
-  } else {
-    if(userGuess > computerGuess) {
-      document.getElementById('textOutput').innerHTML = 'You lose!' + '<br> the number was ' + computerGuess;
-      document.getElementById('container').style.backgroundColor = '#e82c4e';
-      gameEnded();
-    } else if (userGuess < computerGuess) {
-      document.getElementById('textOutput').innerHTML = 'You lose!' + '<br> the number was ' + computerGuess;
-      document.getElementById('container').style.backgroundColor = '#e82c4e';
-      gameEnded();
-    } else {
-      document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' +attempts+ ' attempts';
-      document.getElementById('container').style.backgroundColor = 'green';
-      gameEnded();
-    }
-  }
 
 
 }
