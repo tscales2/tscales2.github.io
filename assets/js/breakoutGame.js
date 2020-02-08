643c9cc9b9f20a4e4b05b5d1ca2e16403f94f793
tscales2 <takinahmscales@gmail.com>
2020-02-08 01:22:41 -0500

    made the projects viewable and playable. also added fourth project

--- assets/js/breakoutGame.js
+++ assets/js/breakoutGame.js
@@ -70,7 +70,7 @@ function collisionDetection() {
 
                     score++;
                     if (score == brickRowCount * brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.getElementById('textOutput').innerHTML = 'YOU WIN, CONGRATULATIONS!';
                         document.location.reload();
 
                     }
@@ -139,31 +139,28 @@ function draw() {
     drawBricks();
     drawScore();
     drawLives();
-     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if(y + dy < ballRadius) {
-    dy = -dy;
-  }
-  else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
     }
-    else {
-      lives--;
-      if(!lives) {
-        alert("GAME OVER");
-        document.location.reload();
-      }
-      else {
-        x = canvas.width/2;
-        y = canvas.height-30;
-        dx = 3;
-        dy = -3;
-        paddleX = (canvas.width-paddleWidth)/2;
-      }
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            lives--;
+            if (!lives) {
+                document.getElementById('textOutput').innerHTML = 'GAME OVER!';
+                document.location.reload();
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
     }
-  }
 
     if (rightPressed) {
         paddleX += 7;
